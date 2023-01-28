f9c14f4908298e5bdd69b33844378c2db4744101
jamylguimaraes <jamylguimaraes@gmail.com>
2023-01-28 15:24:41 -0300

    Funcao criada para download, instalacao e configuracao do jdk

--- files/lab1.py
+++ files/lab1.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python2
 import os
 
+ef java_install():
+	os.system('wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm')
+	print("Download do Java Completo!")
+	os.system('yum -y localinstall jdk-17_linux-x64_bin.rpm')
+	print("Instalacao do Java Completa!")
+	os.system('cp java_home.sh /etc/profile.d/')
+	os.system('source ~/.bash_profile')	
+
 # Verifica se o wget existe na vm, se nao instala com yum
 # Realiza o download do java no site da oracle e prosegue com a instalacao 
 if os.system('wget') != 0 :
 	os.system('yum -y install wget')
-	os.system('wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm')
-	print("Download do Java Completo!")
-	os.system('yum -y localinstall jdk-17_linux-x64_bin.rpm')
-	print("Instalacao do Java Completa!")
-	os.system('cp java_home.sh /etc/profile.d/')
-	os.system('source ~/.bash_profile')
+	java_install()
 else :
-	os.system('wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm')
-	print("Download do Java Completo!")
-	os.system('yum -y localinstall jdk-17_linux-x64_bin.rpm')
-	print("Instalacao do Java Completa!")
-	os.system('cp java_home.sh /etc/profile.d/')
-	os.system('source ~/.bash_profile')
+	java_install()
 
 
 
