74d5bbf651a7e955dcaee774c159a3332a690b8b
jamylguimaraes <jamylguimaraes@gmail.com>
2023-01-30 00:22:52 -0300

    Inclusao de funcao para alteracao de arquivo xml, criacao de variaveis para armazenamento de paramentros(urls)

--- files/lab1.py
+++ files/lab1.py
@@ -3,38 +3,50 @@ import os
 import tarfile
 import xml.etree.ElementTree as ET
 
+java_url = "https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm"
+zeppelin_url = "https://dlcdn.apache.org/zeppelin/zeppelin-0.10.1/zeppelin-0.10.1-bin-all.tgz"
+
 def java_install():
 	if os.path.exists('jdk-17_linux-x64_bin.rpm') :
 		print("Download do JDK ja existe")
 	else :
-		os.system('wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm')
+		os.system('wget --no-check-certificate -c {}' .format(java_url)) 
 		print("Download do Java Completo!")
 		os.system('yum -y localinstall jdk-17_linux-x64_bin.rpm')
 		print("Instalacao do Java Completa!")
-		os.system('cp java_home.sh /etc/profile.d/')
+		os.system('cp /home/vagrant/files/java_home.sh /etc/profile.d/')
 		os.system('source ~/.bash_profile')	
 
 def zeppelin_xml_update():
-	tree = ET.parse('/opt/zeppelin/conf/zeppelin-site.xml.template')
+	tree = ET.parse('/opt/zeppelin/conf/zeppelin-site.xml')
 	root = tree.getroot()
 	ip = root[0][1]
 	ip.text = '192.168.56.10'
+	port = root[1][1]
+	port.text = '8888'
 	tree.write('/opt/zeppelin/conf/zeppelin-site.xml')
 
 def zeppelin_install():
 	if os.path.exists('zeppelin-0.10.1-bin-all.tgz') :
 		print("Download do Zeppelin ja existe")
 	else :
-		os.system('wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://dlcdn.apache.org/zeppelin/zeppelin-0.10.1/zeppelin-0.10.1-bin-all.tgz')
+		os.system('wget --no-check-certificate -c %s' % (zeppelin_url)) 
 		print("Download do Zeppelin Completo!")
 		tar = tarfile.open('zeppelin-0.10.1-bin-all.tgz')
 		tar.extractall()
 		tar.close()
-		os.system('mv zeppelin-0.10.1-bin-all/ /opt/zeppelin')
+		os.system('mkdir -p /opt/zeppelin')
+		os.system('mv /home/vagrant/zeppelin-0.10.1-bin-all/* /opt/zeppelin/')
 		os.system('adduser -d /opt/zeppelin -s /sbin/nologin zeppelin')
 		os.system('chown -R zeppelin:zeppelin /opt/zeppelin')
-		os.system('cp systemd/zeppelin.service /etc/systemd/system/')
+		os.system('cp /home/vagrant/files/systemd/zeppelin.service /etc/systemd/system/')
+		os.system('chmod 755 /etc/systemd/system/zeppelin.service')
+		os.system('cp /home/vagrant/files/conf/shiro.ini /opt/zeppelin/conf/')
+		os.system('cp /opt/zeppelin/conf/zeppelin-site.xml.template /opt/zeppelin/conf/zeppelin-site.xml')
 		zeppelin_xml_update()
+		os.system('systemctl enable zeppelin')
+		os.system('systemctl start zeppelin')
+
 
 # Verifica se o wget existe na vm, se nao instala com yum
 # Realiza o download do java no site da oracle e prosegue com a instalacao 
